aae103892702959e19a4d9eaff80e1a820c9e982
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2018-10-21 16:18:20 +0200

    initial commit

---
+++ C/001_two_sum/two_cycles.c
@@ -0,0 +1,12 @@
+/*** Note: The returned array must be malloced, assume caller calls free().*/
+int* twoSum(int* nums, int numsSize, int target) 
+{
+	int* r = malloc (sizeof(int)*2);
+	for (int i = 0; i < numsSize - 1; ++i)
+		for (int j = i + 1; j < numsSize; ++j)
+			if (nums[i] + nums[j] == target) {
+				r[0] = i;r[1] = j;
+				return r;
+			}
+	return -1;
+}
