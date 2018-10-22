bf056f25d45626b921e78793e712a8b81c643723
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2018-10-22 23:29:36 +0200

    initial commit, needs polishing

---
+++ C/003_longest_substring/first_try.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 300
+
+int hash (int key)
+{	
+	int index = key%SIZE;
+	return index < 0 ? index + SIZE : index;
+}
+
+int search (int *indexes, int key)
+{
+	int index = hash (key);
+	if (indexes[index] == key)
+		return 1;
+	return 0;
+}
+
+int insert (int key, int *indexes) 
+{
+	int index = hash(key);
+	if (indexes[index] == key)
+		return 0;	
+	else
+	{
+		indexes[index] = key;
+		return 1;
+	}
+	return 0;
+}
+int lengthOfLongestSubstring(char* s) 
+{
+	char key;
+	int *indexes = malloc(sizeof(int)*SIZE);
+	int i = 0, max_length_count = 0, current_length = 0;
+	while ((key = s[i]) != '\0')
+	{
+		i++;
+		if (insert((key - '0'), indexes))
+		{	
+			current_length++;
+			continue;
+		}
+		if (current_length > max_length_count)
+			max_length_count = current_length;
+		current_length = 0;
+		for (int j = 0; j <= i; ++j)
+			indexes[j] = 0;
+	}
+	return max_length_count;
+}
+
+int main(void)
+{
+	int number = lengthOfLongestSubstring("abcbbb");
+	printf("%d\n", number);
+	return 0;
+}
