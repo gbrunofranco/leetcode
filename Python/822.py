c4ac92ef94280e6450d8ba3c1e6bcf13fc969281
Gabriele Bruno Franco <gabrielebrunofranco@gmail.com>
2021-08-31 09:41:26 +0200

    feat: time 169ms 16.96%, memory 14MB 16.96%

---
+++ Python/822.py
@@ -0,0 +1,14 @@
+from typing import List, Set
+
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        invalid = (front_elem for idx, front_elem in enumerate(fronts) if front_elem == backs[idx])
+        complete = (set(fronts) | set(backs)) - set(invalid) 
+        if len(complete) == 0:
+            return 0
+        return sorted(complete)[0]
+
+if __name__ == "__main__":
+    ex = Solution()
+
+    print(ex.flipgame([1,2,4,4,7], [1,3,4,1,3]))
